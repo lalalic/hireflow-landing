4b3655bab8aa1cb047182fc8ee210d9e3e5eaf7f
Cheng Li <lalalic@gmail.com>
2026-03-14 12:33:07 -0400

    chore: update sitemap with /course and /privacy pages

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -9,5 +9,17 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'weekly',
       priority: 1,
     },
+    {
+      url: `${baseUrl}/course`,
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/privacy`,
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.3,
+    },
   ];
 }
